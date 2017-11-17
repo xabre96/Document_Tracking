121beff16cc80e7529fd5c506dc6c9b4f59b843c
xabre96 <xan.gutierrez@gmail.com>
2017-11-17 22:41:54 +0800

    Fixed merge conflict issues.

--- application/config/config.php
+++ application/config/config.php
@@ -1,6 +1,6 @@
 <?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
 
-$config['base_url']	= 'http://localhost/dms';
+$config['base_url']	= 'http://localhost/dms2';
 $config['index_page'] = '';
 $config['uri_protocol']	= 'AUTO';
 $config['url_suffix'] = '.html';
