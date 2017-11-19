d1e081e48f4992aa85767634879b19cf3ad3f1d7
xabre96 <xan.gutierrez@gmail.com>
2017-11-19 15:47:51 +0800

    Logout route added.

--- application/config/routes.php
+++ application/config/routes.php
@@ -3,4 +3,8 @@
 $route['default_controller'] = "users_controller/index";
 $route['404_override'] = 'error/404';
 
+/**
+ * users_controller routes
+ */
 $route['login'] = 'users_controller/userLogin';
+$route['logout'] = 'users_controller/logout';
