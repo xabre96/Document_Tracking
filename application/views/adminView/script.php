66378ae501832dc0594a2f1188f47cf272f3e652
xabre96 <xan.gutierrez@gmail.com>
2017-12-13 21:54:49 +0800

    Weekends date checking added.

--- application/views/adminView/script.php
+++ application/views/adminView/script.php
@@ -44,6 +44,38 @@
     var month = currentDate.getMonth() + 1;
     var year = currentDate.getFullYear().toString().slice(2);
 
+    function checkDateIfWeekends(dueAndFollowDate, documentType) {
+        var daysToAdd = 0;
+        var newDueAndFollowDate = dueAndFollowDate;
+        var day = moment(dueAndFollowDate).day();
+
+        if (documentType === 1) { // Document type is rush
+            switch (day) {
+                case 0: // day is Sunday
+                    daysToAdd = 1;
+                    break;
+                case 6: // day is Saturday
+                    daysToAdd = 2;
+                    break;
+            }
+        }
+
+        if (documentType === 3) { // Document type is routinary
+            switch (day) {
+                case 0: // day is Sunday
+                case 1: // day is Monday
+                case 2: // day is Tuesday
+                case 6: // day is Saturday
+                    daysToAdd = 2;
+                    break;
+            }
+        }
+
+        newDueAndFollowDate = moment(dueAndFollowDate).add(daysToAdd, 'days').format('YYYY-MM-DD');
+
+        return newDueAndFollowDate;
+    }
+
     $(".document-id").text(year + '-' + month + '-' + documentId);
 
     $("#saveBtn").click(function () {
@@ -61,11 +93,13 @@
         switch (parseInt($("#complianceType").val())) {
             case 1:
                 var dueAndFollowDate = moment(tempDate).add(1, 'days').format('YYYY-MM-DD');
+                dueAndFollowDate = checkDateIfWeekends(dueAndFollowDate, 1);
                 $("#dueDate").val(dueAndFollowDate);
                 $("#followDate").val(dueAndFollowDate);
                 break;
             case 3:
                 var dueAndFollowDate = moment(tempDate).add(4, 'days').format('YYYY-MM-DD');
+                dueAndFollowDate = checkDateIfWeekends(dueAndFollowDate, 3);
                 $("#dueDate").val(dueAndFollowDate);
                 $("#followDate").val(dueAndFollowDate);
                 break;
